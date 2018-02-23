a1d568d9929bceccd133b5fc71d36487f6d5ab20
Tony Wasserka <neobrainx@gmail.com>
2018-02-23 23:28:06 +0000

    Add support for (displaying) comments
    Also supports getting avatars from Gravatar.

--- site.hs
+++ site.hs
@@ -1,8 +1,13 @@
 --------------------------------------------------------------------------------
 {-# LANGUAGE OverloadedStrings #-}
-import           Data.Monoid (mappend)
-import           Hakyll
+import Control.Monad (filterM)
+import Data.Maybe (fromJust, fromMaybe, isNothing)
+import Data.Monoid (mappend)
+import Data.List (reverse, stripPrefix)
+import qualified Data.Text as T (pack)
+import Hakyll
 
+import qualified Network.Gravatar as Gravatar
 
 --------------------------------------------------------------------------------
 main :: IO ()
@@ -21,13 +26,70 @@ main = hakyll $ do
             >>= loadAndApplyTemplate "templates/default.html" defaultContext
             >>= relativizeUrls
 
-    match "posts/*" $ do
+    match "posts/*.markdown" $ do
         route $ setExtension "html"
-        compile $ pandocCompiler
+        compile $
+            do
+              resource <- getResourceString
+              let ident = itemIdentifier resource
+              let filename = reverse $ fromJust $ stripPrefix "nwodkram." $ reverse $ fromJust $ stripPrefix "posts/" $ toFilePath ident
+              comments <- loadAll $ fromGlob $ "blog_comments/" ++ filename ++ "/*"
+
+              -- Top level comments = comments without replies
+              let isTopLevelComment comment = do
+                    parentId <- getMetadataField (itemIdentifier comment) "parent_id"
+                    return $ isNothing parentId
+
+              let getChildrenOf potentialChildren comment = do
+                    Just comment_id <- getMetadataField (itemIdentifier comment) "comment_id"
+                    let isChild potentialChild = do
+                          parentId <- getMetadataField (itemIdentifier potentialChild) "parent_id"
+                          return $ maybe False (comment_id==) parentId
+                    filterM isChild potentialChildren
+
+              -- Build a hierachy of comments so we can sort replies and toplevel comments independently
+              commentsHierarchy <- do
+                topLevelComments <- filterM isTopLevelComment comments
+                children <- mapM (getChildrenOf comments) topLevelComments
+                return $ zip topLevelComments children
+
+              -- TODO: Sort comments within the hierarchy by date!
+
+              -- Enumerate comment indices
+              let topLevelIndices = scanl (\index (_, children) -> succ index + length children) 1 commentsHierarchy
+
+              -- Fill in template placeholders for childs (i.e. comment index only)
+              let transformChildComment (comment, commentIndex) = do
+                    let newCtx =  constField "comment_index" (show commentIndex) `mappend`
+                                  defaultContext
+                    applyAsTemplate newCtx comment
+
+              -- Fill in template placeholders for childs (i.e. comment index and replies)
+              let transformTopLevelComment ((comment, children), commentIndex) = do
+                    processedChildren <- mapM transformChildComment $ zip children [succ commentIndex..]
+                    Just comment_id <- getMetadataField (itemIdentifier comment) "comment_id"
+                    let newCtx =  constField "comment_index" (show commentIndex) `mappend`
+                                  constField "is_toplevel" "1" `mappend`
+                                  defaultContext
+
+                    -- Add replies unless there are none
+                    let newCtx' = if null processedChildren
+                                  then newCtx
+                                  else newCtx `mappend` listField "child_comments" defaultContext (return processedChildren)
+
+                    applyAsTemplate newCtx' comment
+
+              processedComments <- mapM transformTopLevelComment $ zip commentsHierarchy topLevelIndices
+
+              let fullCtx = constField "num_comments" (show $ pred $ last topLevelIndices) `mappend` -- count both toplevel comments and replies
+                            listField "comments" postCtx (return processedComments) `mappend`
+                            postCtx
+
+              pandocCompiler
                 >>= saveSnapshot "content" -- save snapshot for teaser text
-                >>= loadAndApplyTemplate "templates/post.html"    postCtx
+                >>= loadAndApplyTemplate "templates/post.html"    fullCtx -- TODO: May need to split off comments now...
                 >>= saveSnapshot "content"
-                >>= loadAndApplyTemplate "templates/default.html" postCtx
+                >>= loadAndApplyTemplate "templates/default.html" fullCtx
                 >>= relativizeUrls
 
     match "talks/*markdown" $ do
@@ -92,6 +154,22 @@ main = hakyll $ do
                 >>= relativizeUrls
 
     match "templates/*" $ compile templateBodyCompiler
+    match "blog_comments/*/*.markdown" $ compile $ do
+      -- TODO: Verify the comment_ids for all comments are unique
+      comment <- getResourceString
+      email <- getMetadataField (itemIdentifier comment) "email"
+      Just name <- getMetadataField (itemIdentifier comment) "name"
+      let commentCtx =  defaultContext `mappend`
+                        -- Generate link to gravatar using the given email address; if none was specified, generate a dummy image from the name
+                        (constField "gravatar_url" . Gravatar.gravatar gravatarConfig . T.pack) (fromMaybe ("dummy_email_" ++ name) email)
+                        where gravatarConfig = Gravatar.def { Gravatar.gDefault = Just Gravatar.Identicon }
+
+      -- Preprocessing body: This will fill in all placeholders other than the comment index
+      -- We also make sure to escape special HTML characters here
+      resource <- getResourceBody
+      withItemBody (return . escapeHtml) resource
+        >>= loadAndApplyTemplate "templates/blog_comment.html" commentCtx
+        >>= relativizeUrls
 
 
 --------------------------------------------------------------------------------
